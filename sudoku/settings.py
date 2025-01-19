fe97cc4919c828ae9bc254751ce74497a247376a
damianludwiczak89 <damian_konin@tlen.pl>
2025-01-19 21:51:20 +0100

    secret key

--- sudoku/settings.py
+++ sudoku/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#ec(b5miklkjb&r^#7w$73ri1nl3ib_d1@3oxs8#6t)atd4%%n"
+SECRET_KEY = os.environ['SECRET-KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
